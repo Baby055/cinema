6d6782620261d322e3bf528789038ea771b17f53
Baby055 <hei.sombiniaina@gmail.com>
2026-08-01 10:40:37 +0300

    feat(RoomRepository) : added with 3 methods(findAll, findById, save)

---
+++ src/main/java/school/hei/demo/repository/RoomRepository.java
@@ -0,0 +1,46 @@
+package school.hei.demo.repository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import school.hei.demo.model.Room;
+import school.hei.demo.repository.model.JRoom;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+@AllArgsConstructor
+public class RoomRepository {
+    private final JRoomRepository jRoomRepository;
+
+    public List<Room> findAll() {
+        return jRoomRepository.findAll().stream()
+                .map(RoomRepository::toDomain)
+                .toList();
+    }
+
+    public Optional<Room> findById(UUID id) {
+        return jRoomRepository.findById(id).map(RoomRepository::toDomain);
+    }
+
+    public Room save(Room room) {
+        return toDomain(jRoomRepository.save(toEntity(room)));
+    }
+
+    public static Room toDomain(JRoom entity) {
+        return new Room(
+                entity.getId(),
+                entity.getNumber(),
+                entity.getCapacity()
+        );
+    }
+
+    public static JRoom toEntity(Room room) {
+        return new JRoom(
+                room.getId(),
+                room.getNumber(),
+                room.getCapacity()
+        );
+    }
+}
