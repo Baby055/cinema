14fc4ddd7c7368b02e3978f01a50e259aad38b9d
Baby055 <hei.sombiniaina@gmail.com>
2026-08-05 19:18:26 +0300

    style(format.sh)

--- src/main/java/school/hei/demo/endpoint/rest/model/SaveMovie.java
+++ src/main/java/school/hei/demo/endpoint/rest/model/SaveMovie.java
@@ -2,7 +2,6 @@ package school.hei.demo.endpoint.rest.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import java.util.Set;
 import java.util.UUID;
--- src/main/java/school/hei/demo/repository/JUserRepository.java
+++ src/main/java/school/hei/demo/repository/JUserRepository.java
@@ -2,9 +2,7 @@ package school.hei.demo.repository;
 
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import school.hei.demo.repository.model.JUser;
 
 public interface JUserRepository extends JpaRepository<JUser, UUID> {
