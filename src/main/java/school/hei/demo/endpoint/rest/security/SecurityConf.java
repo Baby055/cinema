866725203c431de2dd0ad9d7a33e577105c6e768
Baby055 <hei.sombiniaina@gmail.com>
2026-08-07 16:39:23 +0300

    style(format.sh)

--- src/main/java/school/hei/demo/endpoint/rest/security/SecurityConf.java
+++ src/main/java/school/hei/demo/endpoint/rest/security/SecurityConf.java
@@ -34,33 +34,30 @@ public class SecurityConf {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf(csrf -> csrf.disable())
-            .sessionManagement(
-                    session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(
-                    authorize ->
-                            authorize
-                                    .requestMatchers("/ping", "/health/**")
-                                    .permitAll()
-                                    .requestMatchers(HttpMethod.GET, "/projections/**")
-                                    .permitAll()
-                                    .requestMatchers(HttpMethod.GET, "/movies/**")
-                                    .permitAll()
-                                    .requestMatchers(HttpMethod.PUT, "/users")
-                                    .permitAll()
-
-                                    .requestMatchers(HttpMethod.PUT, "/movies")
-                                    .hasRole("MANAGER")
-                                    .requestMatchers(HttpMethod.PUT, "/projection")
-                                    .hasRole("MANAGER")
-                                    .requestMatchers(HttpMethod.GET, "/users")
-                                    .hasRole("MANAGER")
-
-                                    .requestMatchers(HttpMethod.GET, "/reservations")
-                                    .hasAnyRole("EMPLOYEE", "MANAGER")
-
-                                    .anyRequest()
-                                    .authenticated())
-            .httpBasic(basic -> {});
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(
+            authorize ->
+                authorize
+                    .requestMatchers("/ping", "/health/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/projections/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.GET, "/movies/**")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/users")
+                    .permitAll()
+                    .requestMatchers(HttpMethod.PUT, "/movies")
+                    .hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.PUT, "/projection")
+                    .hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.GET, "/users")
+                    .hasRole("MANAGER")
+                    .requestMatchers(HttpMethod.GET, "/reservations")
+                    .hasAnyRole("EMPLOYEE", "MANAGER")
+                    .anyRequest()
+                    .authenticated())
+        .httpBasic(basic -> {});
     return http.build();
   }
 }
